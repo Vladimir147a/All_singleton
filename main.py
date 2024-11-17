1d16b99e8f662736b1eaeac0bf4d2534fd28a2c5
dEN5 <moyadres_1@mail.ru>
2024-11-17 12:46:41 +0300

    add python realisation SSSsingleton

---
+++ main.py
@@ -0,0 +1,20 @@
+class SSSsingleton:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self.initialized = True
+
+
+
+
+
+
+
+
+
